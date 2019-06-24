b726a5d6bfdaed0b7d8304f2deddde287ea9ad71
aerionTech <info@aerion.com.au>
2019-06-24 15:18:30 +1000

    Added watermark parameter

--- ios/RSSignatureViewManager.h
+++ ios/RSSignatureViewManager.h
@@ -3,7 +3,7 @@
 
 @interface RSSignatureViewManager : RCTViewManager
 @property (nonatomic, strong) RSSignatureView *signView;
--(void) saveImage:(nonnull NSNumber *)reactTag;
+-(void) saveImage:(nonnull NSNumber *)reactTag withWatermark:(NSString*)watermark;
 -(void) resetImage:(nonnull NSNumber *)reactTag;
 -(void) publishSaveImageEvent:(NSString *) aTempPath withEncoded: (NSString *) aEncoded;
 -(void) publishDraggedEvent;
